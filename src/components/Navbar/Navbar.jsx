970b71047bf54ce5588d55a47efa837b3a83b8ac
seang kong heng <konghengseang878@gmail.com>
2026-01-18 11:17:58 +0700

    change home navbar to home 1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -160,7 +160,7 @@ const Navbar = () => {
             {/* DESKTOP MENU  */}
             <ul className="hidden md:flex items-center gap-8">
               {[
-                { name: "Home Testing", path: "/" },
+                { name: "Home 1", path: "/" },
                 { name: "Features", path: "/feature" },
                 { name: "Products", path: "/product" },
                 { name: "Download", path: "/download" },
